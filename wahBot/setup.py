30a3a59fbebf72aa93d71b02ba671134c25a8aea
Carmelo Sarta <carmelo.sarta.main@gmail.com>
2019-05-07 11:19:39 +0200

    Add files via upload

---
+++ wahBot/setup.py
@@ -0,0 +1,40 @@
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+
+def start(bot, update):
+    bot.send_message(update.message.chat_id, text='WAH')
+
+
+def answer(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='WAH')
+
+
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that WAH! Speak clearly, bitch!")
+
+
+def main():
+    ciccia = telegram.Bot(token='771479009:AAGugUnJgzBbraCDhXMtcdByHoYDTsMnxDY')
+    print(ciccia.get_me())
+
+    updater = Updater(token='771479009:AAGugUnJgzBbraCDhXMtcdByHoYDTsMnxDY')
+
+    dispatcher = updater.dispatcher
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    answ_handler = MessageHandler(Filters.regex('gay'), answer)
+    dispatcher.add_handler(answ_handler)
+
+    unk_handler = MessageHandler(Filters.text, unknown)
+    dispatcher.add_handler(unk_handler)
+
+    updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()
